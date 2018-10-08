b29fc20d6c8b25aafc7b85dbb5365398fce33a46
Evelina <evelinasutiene@yahoo.com>
2018-10-08 19:44:06 +0300

    pirmas ikelimas

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ManoKlase objektas = new ManoKlase ();
+
+        objektas.spausdinkvarda ();
+
+
+
+    }
+}
